7da251c1b71c2e74d2233dbf3a90f4693683b786
Balaji-Kintala <kintalabalaji03@gmail.com>
2026-03-13 22:17:07 +0530

    chore: prepare for unified deployment on Render

--- server.js
+++ server.js
@@ -59,6 +59,15 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Serve Frontend in Production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
